30854ac1a77423e6e50aa0c9270a3ff92a6e10f4
ylin2150@use.edu <ylin2150@use.edu>
2020-05-07 22:17:46 +0800

    updata solution.cc

--- Find_median/src/lib/solution.cc
+++ Find_median/src/lib/solution.cc
@@ -8,14 +8,27 @@ double Solution::FindMedian(std::vector<double> &inputs) {
     return -1;
   }
   double result;
-  std:: sort(inputs.begin(),inputs.end());
-    if (inputs.size()%2==1) {
-      result=(double)inputs[inputs.size()/2];
+  // std:: sort(inputs.begin(),inputs.end());
+  //   if (inputs.size()%2==1) {
+  //     result=(double)inputs[inputs.size()/2];
+  //   }
+  //   else{
+  //     result=(double) (inputs[(inputs.size()-1)/2]+inputs[inputs.size()/2])/2;
+  //     //std::cout <<(double) (inputs[(inputs.size()-1)/2]+inputs[inputs.size()/2])/2<< std ::endl;
+  //   }
+  // return result;
+
+  // more efficient ways
+  int size=inputs.size();
+  if(size!=0){
+    if (size %2 == 0){
+      return(inputs[size/2-1]+inputs[size/2+1]) /2;
     }
     else{
-      result=(double) (inputs[(inputs.size()-1)/2]+inputs[inputs.size()/2])/2;
-      //std::cout <<(double) (inputs[(inputs.size()-1)/2]+inputs[inputs.size()/2])/2<< std ::endl;
+      return inputs[size/2];
     }
-  return result;
+  }
+  else{
+    return 0;
+  }
 }
-
